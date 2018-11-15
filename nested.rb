93cb623189f9836523d8a170b00ea2dbef76a3a3
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-15 14:20:30 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
 
@@ -82,7 +82,7 @@ def adding_matz
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => "C"
+          :languages => ["C"]
         }
      }
 
