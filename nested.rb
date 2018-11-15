ef80df09aa016db057adde5e533431f19d4cf51a
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-15 14:56:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -86,7 +85,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
 	programmer_hash[:yukihiro_matsumoto] = {
 		                :known_for => "Ruby",
 										:languages => ["LISP", "C"]
@@ -114,8 +112,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
-		 
-
+  return programmer_hash
 end
 
 def adding_to_dennis
